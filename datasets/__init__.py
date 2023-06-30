6960b313bde4ecf635d39a935433727d686d1e4e
AlArgente <albertoargentedcgarrido@gmail.com>
2023-06-30 11:40:32 +0200

    Structure modified

--- datasets/__init__.py
+++
@@ -1,8 +0,0 @@
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-
-from flextrees.datasets.tabular_datasets import credit2
-from flextrees.datasets.tabular_datasets import nursery
-from flextrees.datasets.tabular_datasets import adult
-from flextrees.datasets.tabular_datasets import bank
--- pool/__init__.py
+++
@@ -1,5 +0,0 @@
-from __future__ import absolute_import
-from __future__ import division
-from __future__ import print_function
-
-from flextrees.pool.aggregators_fedid3 import id3_agg
