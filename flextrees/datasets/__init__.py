767fef6334d9f2d17748729957afe01f8c8e1ef8
cristinazuhe <zuhe18@gmail.com>
2024-01-30 08:48:52 +0100

    Updated init files

--- flextrees/datasets/__init__.py
+++ flextrees/datasets/__init__.py
@@ -1,6 +1,6 @@
-from __future__ import absolute_import
 from __future__ import division
 from __future__ import print_function
+from __future__ import absolute_import
 
 from flextrees.datasets.tabular_datasets import credit2
 from flextrees.datasets.tabular_datasets import nursery
